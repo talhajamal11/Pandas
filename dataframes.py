2b3f4558e1a4358e7bf5ef449d74ac8da63de270
Talha Jamal <talhajamal11@gmail.com>
2023-05-01 02:58:06 +0500

    Fixing up things

--- dataframes.py
+++ dataframes.py
@@ -14,38 +14,33 @@ series = pd.Series(["Talha", 26, "Male"],
                    index=["Name", "Age", "Sex"],
                    name="Talha metadata"
                    )
-#print(df)
-#print("-----------")
-#print(series)
+print(df)
+print("-----------")
+print(series)
 
 #print("-----------")
 path = str(os.getcwd()) + "/sample.csv"
 read_file = pd.read_csv(path)
 read_file.columns = ["Product_A", "Product_B","Product_C"]
 
-#print(read_file)
-#print(read_file.shape)
+print(read_file)
+print(read_file.shape)
 
 # Save df to file
-
 read_file.to_csv("read_file.csv")
 
-
 # using the Loc method on Pandas DF
-#print(read_file.iloc[:, 1])
+print(read_file.iloc[:, 1])
 
 new_df = read_file.loc[(read_file.Product_A > 30)]
 
 print("*******************")
-#print(new_df)
+print(new_df)
 
 print(new_df)
 
 def square(x):
     return x**2
-
-
-print("*******************")
 
 row_1 = new_df.Product_A.map(square)
 row_2 = new_df.Product_B.map(square)
