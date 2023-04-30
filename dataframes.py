2f29418fc8224f277a681bb2617970bea2384262
Talha Jamal <talhajamal11@gmail.com>
2023-05-01 02:48:26 +0500

    trying out maps and summaries

--- dataframes.py
+++ dataframes.py
@@ -23,8 +23,8 @@ path = str(os.getcwd()) + "/sample.csv"
 read_file = pd.read_csv(path)
 read_file.columns = ["Product_A", "Product_B","Product_C"]
 
-print(read_file)
-print(read_file.shape)
+#print(read_file)
+#print(read_file.shape)
 
 # Save df to file
 
@@ -32,9 +32,25 @@ read_file.to_csv("read_file.csv")
 
 
 # using the Loc method on Pandas DF
-print(read_file.iloc[:, 1])
+#print(read_file.iloc[:, 1])
 
 new_df = read_file.loc[(read_file.Product_A > 30)]
 
 print("*******************")
+#print(new_df)
+
 print(new_df)
+
+def square(x):
+    return x**2
+
+
+print("*******************")
+
+row_1 = new_df.Product_A.map(square)
+row_2 = new_df.Product_B.map(square)
+print(row_1)
+print("*****************")
+print(row_2)
+
+print(new_df.describe())
