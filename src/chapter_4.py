4ad05a2db51cb67e25f00fcb5c6fda3ad440a342
talhajamal11 <talhajamal11@gmail.com>
2023-08-03 17:10:29 +0500

    Answering Chapter 4 Questions

---
+++ src/chapter_4.py
@@ -0,0 +1,18 @@
+"""
+1. Create a series with the temperature values for the last seven days. 
+    Filter out the values below the mean.
+2. Create a series with your favorite colors. Use a categorical type.
+"""
+import pandas as pd
+
+temp = pd.Series(data=[30, 26, 32, 27, 36, 31, 28],
+          index=['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'],
+          dtype='int64',
+          name="Temperature")
+
+print(temp)
+print(f'The Mean Temperature is {temp.mean()} \n')
+low_temp = temp < temp.mean()
+print(temp[low_temp])
+
+
